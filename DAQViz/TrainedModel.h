7fab929bc3c20b9ae75560c7de0398325555bbf9
Sejin Kim <64125036+universe-2030@users.noreply.github.com>
2021-05-17 22:37:49 +0900

    Revised TrainedModel class

--- DAQViz/TrainedModel.h
+++ DAQViz/TrainedModel.h
@@ -7,9 +7,13 @@ class TrainedModel {
 
 // Training model - Sejin Kim
 public:
-
+	void Set_mean_sEMG_motionwise(double** input);
+	void Set_std_sEMG_motionwise(double** input);
 
 private:
+	int N_motions;
+	int N_sEMG;
 
-
+	double** mean_sEMG_motionwise;
+	double** std_sEMG_motionwise;
 };
